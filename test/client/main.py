539d8f8bfb39328db5e30035f5e331ec27ce8299
LosVocelos <janocelik@gmail.com>
2022-12-09 20:18:04 +0100

    JUST ANOTHER BROKEN APP, I'M FUKIN' PISSED

--- test/client/main.py
+++ test/client/main.py
@@ -1,59 +1,38 @@
-'''
-Camera Example
-==============
-
-This example demonstrates a simple use of the camera. It shows a window with
-a buttoned labelled 'play' to turn the camera on and off. Note that
-not finding a camera, perhaps because gstreamer is not installed, will
-throw an exception during the kv language processing.
-
-'''
-
-# Uncomment these lines to see all the messages
-# from kivy.logger import Logger
-# import logging
-# Logger.setLevel(logging.TRACE)
-
+#!/usr/bin/env python
+"""
+This demo can be ran from the project root directory via:
+```sh
+python src/main.py
+```
+It can also be ran via p4a/buildozer.
+"""
 from kivy.app import App
 from kivy.lang import Builder
-from kivy.uix.boxlayout import BoxLayout
-import time
-Builder.load_string('''
-<CameraClick>:
+
+
+DEMO_APP_KV_LANG = """
+#:import ZBarCam kivy_garden.zbarcam.ZBarCam
+#:import ZBarSymbol pyzbar.pyzbar.ZBarSymbol
+BoxLayout:
     orientation: 'vertical'
-    Camera:
-        id: camera
-        resolution: (640, 480)
-        play: False
-    ToggleButton:
-        text: 'Play'
-        on_press: camera.play = not camera.play
-        size_hint_y: None
-        height: '48dp'
-    Button:
-        text: 'Capture'
-        size_hint_y: None
-        height: '48dp'
-        on_press: root.capture()
-''')
+    ZBarCam:
+        id: zbarcam
+        # optional, override the camera index (default 0)
+        camera_index: 0     # or try a different value
+        # optional, by default checks all types
+        code_types: ZBarSymbol.QRCODE, ZBarSymbol.EAN13
+    Label:
+        size_hint: None, None
+        size: self.texture_size[0], 50
+        text: ', '.join([str(symbol.data) for symbol in zbarcam.symbols])
+"""
 
 
-class CameraClick(BoxLayout):
-    def capture(self):
-        '''
-        Function to capture the images and give them the names
-        according to their captured time and date.
-        '''
-        camera = self.ids['camera']
-        timestr = time.strftime("%Y%m%d_%H%M%S")
-        camera.export_to_png("IMG_{}.png".format(timestr))
-        print("Captured")
-
-
-class TestCamera(App):
+class DemoApp(App):
 
     def build(self):
-        return CameraClick()
+        return Builder.load_string(DEMO_APP_KV_LANG)
 
 
-TestCamera().run()
+if __name__ == '__main__':
+    DemoApp().run()
