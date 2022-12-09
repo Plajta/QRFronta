cebc098849722632231a84bfd45a83c532ac5dff
Vesek <venda.straka@gmail.com>
2022-12-09 20:16:20 +0100

    ya

--- src/server/server.py
+++ src/server/server.py
@@ -3,7 +3,7 @@ import socket
 import uuid
 
 # custom import
-from database import *
+from database import redisbase
 
 # Define socket host and port
 SERVER_HOST = '127.0.0.1'
@@ -16,6 +16,8 @@ server_socket.bind((SERVER_HOST, SERVER_PORT))
 server_socket.listen()
 print('Listening on port %s ...' % SERVER_PORT)
 
+database = redisbase()
+
 try:
     while True:    
         # Wait for client connections
@@ -24,22 +26,21 @@ try:
         # Get the client request
         dataFromClient = client_connection.recv(1024).decode()
         response = "OK"
+
         if dataFromClient == "Novej":
-            lidi = Redis_Retrieve() or []
             UUID = uuid.uuid1()
-            #print(str(len(lidi)+1)+", "+str(UUID))
-            lidi.append(UUID)
-            Redis_Set(lidi)
+            # print(str(len(lidi)+1)+", "+str(UUID))
+            database.add(UUID, "pepa")
             response = str(UUID)
         elif dataFromClient == "Starej":
-            retrieved = Redis_Retrieve()
+            retrieved = database.retrieve_all()
             if len(retrieved):
-                Redis_Set(retrieved[:-1])
+                database.set(retrieved[1:])
             response = str(UUID) + "    odebráno"
         elif dataFromClient  == "Zadnej":
-            Redis_delete()
+            database.delete()
 
-        print(Redis_Retrieve())
+        print(database.retrieve_raw())
 
         # Send HTTP response
         #client_connection.sendall(response.encode()) tohle by se pak mohlo hodit
