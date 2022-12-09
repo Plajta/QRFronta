c9ab2e37bb4dd62fa2c525873e8a8f393cae76a3
HelloWorld7894 <danpojhan@protonmail.com>
2022-12-09 16:19:52 +0100

    Přihlašování works, finally...

--- src/server/webserver.py
+++ src/server/webserver.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template
-import os
+from flask import Flask, render_template, request, url_for, flash, redirect
+import yaml
 
 # custom import
 from database import *
@@ -9,13 +9,31 @@ from database import *
 
 app = Flask(__name__)
 
+with open("credentials.yml", "r") as stream:
+        try:
+            data = yaml.safe_load(stream)
+        except yaml.YAMLError as exc:
+            print(exc)
+
 @app.route("/")
 def Response():
     return "Zde nemáš co dělat..."
 
 @app.route("/admin")
 def admin_load():
-    return render_template("index.html")
+    return render_template("login.html")
+
+@app.route("/login", methods = ['POST'])
+def login():
+    
+    username = request.form['username']
+    password = request.form["password"]
+    
+    if username == data["username"] and password == data["password"]:
+        return render_template("index.html")
+    else:
+        return "Nepovedlo se :(, špatné jméno nebo heslo"
+
 
 if __name__ == "__main__":
     app.run()
