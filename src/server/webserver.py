151f50905e782bfcf38fa7cffd28a8b08500ea3f
HelloWorld7894 <danpojhan@protonmail.com>
2022-12-10 04:02:28 +0100

    Admin panel Plně funkční

--- src/server/webserver.py
+++ src/server/webserver.py
@@ -106,5 +106,9 @@ def abort(data):
 
     DB_Check(socketio, Abort)
 
+@socketio.on("delete-specific")
+def remove_specific(data):
+    redisbase.delete(data)
+
 if __name__ == "__main__":
     socketio.run(app, host="127.0.0.1", port=5000)
